c514dfa4e9220b83f86455a77da0662e2f63e95b
serkankarisan <serkankarisan1905@gmail.com>
2024-11-16 20:56:30 +0300

    Ocelot Gateway eklendi.

---
+++ FirstMicroservice.Ocelot.Gateway/Program.cs
@@ -0,0 +1,13 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot();
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.UseOcelot().Wait();
+
+app.Run();
